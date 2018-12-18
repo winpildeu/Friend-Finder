1ea4baf0dfa78ed299e2fbef13e5314edbc3eb91
Winfield H <winterfreshness@gmail.com>
2018-12-18 18:10:48 -0500

    Survey now receives the info of the closest match.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,6 +1,20 @@
 // import our friends array into this file
 let friendsData = require('../data/friends');
 
+// functions ========================================
+
+function calcScore(person) {
+    let scoreArray = person.scores.map(index => {
+        return parseInt(index);
+    });
+
+    let totalScore = scoreArray.reduce((total, num) => {
+        return total + num;
+    });
+
+    return totalScore;
+}
+
 // routing ==========================================
 
 module.exports = function(app) {
@@ -11,7 +25,38 @@ module.exports = function(app) {
       });
 
     app.post("/api/friends", (req, res) => {
-        // confirm input
-        console.log(req.body);
+        // save the input
+        let user = req.body;
+
+        // calculate the user's score
+        let userScore = calcScore(user);
+        console.log(`User's score: ${userScore}`);       
+
+        // make an array of scores for all of the friends
+        let friendsScoreArray = [];
+        friendsData.forEach(index => {
+            friendsScoreArray.push(calcScore(index));
+        });
+
+        // make an array of score differences compared with the user
+        let comparisonArray = friendsScoreArray.map(index => {
+            return Math.abs(userScore - index);
+        });
+        console.log(`Comparison Array: ${comparisonArray}`);
+
+        // find the lowest number (closest match) and save the index of it
+        let minNum = 100;
+        comparisonArray.forEach(index => {
+            (index < minNum) ? minNum = index : null
+        });
+        let match = comparisonArray.indexOf(minNum);
+        console.log(`minNum: ${minNum}`);      
+        console.log(`Your match is index: ${match}`);
+        
+        // add the user's info to the DB
+        friendsData.push(user);
+        
+        // send the profile of the closest match
+        res.send(friendsData[match]);
     });
 };
