1e6ab99a60189ad0ed7c88b0071fe8413868c083
Winfield H <winterfreshness@gmail.com>
2018-12-17 19:02:08 -0500

    jQuery is sending info to the apiRoute.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,8 +1,17 @@
 // import our friends array into this file
 let friendsData = require('../data/friends');
 
-// routing
+// routing ==========================================
 
 module.exports = function(app) {
 
+    // see all of the friends
+    app.get("/api/friends", function(req, res) {
+        res.json(friendsData);
+      });
+
+    app.post("/api/friends", (req, res) => {
+        // confirm input
+        console.log(req.body);
+    });
 };
--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // import the routes
-// require("./app/routing/apiRoutes")(app);
+require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
 
 // start up the server
